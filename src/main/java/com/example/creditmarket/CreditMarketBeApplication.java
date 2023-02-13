38400d6bc3af97311061af72237fabe8a9ebccbe
devSojoong <sojoong.dev@gmail.com>
2023-02-13 21:19:51 +0900

    Conf: initialize spring boot project
    스프링 프로젝트 생성.

---
+++ src/main/java/com/example/creditmarket/CreditMarketBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditMarketBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditMarketBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/creditmarket/CreditMarketBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creditmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditMarketBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
