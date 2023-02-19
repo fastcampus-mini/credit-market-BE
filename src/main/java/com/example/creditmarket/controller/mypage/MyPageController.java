11ab77a49394b32d2d94048b12269c375c3c5368
JeYeongR <wpdud2003@gmail.com>
2023-02-19 18:26:46 +0900

    feat: #6 관심 상품 목록 조회 api 구현

---
+++ src/main/java/com/example/creditmarket/controller/mypage/MyPageController.java
@@ -0,0 +1,24 @@
+package com.example.creditmarket.controller.mypage;
+
+import com.example.creditmarket.dto.FavoriteResponseDTO;
+import com.example.creditmarket.service.mypage.MyPageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MyPageController {
+
+    private final MyPageService myPageService;
+
+    //토큰 받기전에 임시로 email 받기
+    @GetMapping("/favor/{page}")
+    public List<FavoriteResponseDTO> selectFavoriteList(@PathVariable int page, String userEmail) {
+        return myPageService.selectFavoriteList(page, userEmail);
+    }
+
+}
