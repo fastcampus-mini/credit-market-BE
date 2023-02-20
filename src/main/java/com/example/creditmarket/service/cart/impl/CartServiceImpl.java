b8f8e4791a26b0f1e97f3e7f740eb48ecc02f050
JeYeongR <wpdud2003@gmail.com>
2023-02-20 16:18:24 +0900

    feat: #16 임시로 만들어 놓은 Exception 수정

--- src/main/java/com/example/creditmarket/service/cart/impl/CartServiceImpl.java
+++ src/main/java/com/example/creditmarket/service/cart/impl/CartServiceImpl.java
@@ -6,6 +6,8 @@ import com.example.creditmarket.dto.CartSaveRequestDTO;
 import com.example.creditmarket.entity.EntityCart;
 import com.example.creditmarket.entity.EntityFProduct;
 import com.example.creditmarket.entity.EntityUser;
+import com.example.creditmarket.exception.AppException;
+import com.example.creditmarket.exception.ErrorCode;
 import com.example.creditmarket.repository.CartRepository;
 import com.example.creditmarket.repository.FProductRespository;
 import com.example.creditmarket.repository.FavoriteRepository;
@@ -14,6 +16,7 @@ import com.example.creditmarket.service.cart.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,11 +40,7 @@ public class CartServiceImpl implements CartService {
 
         String fproductId = cartRequestDTO.getFproductId();
         EntityFProduct fProduct = fProductRespository.findById(fproductId)
-                .orElseThrow(() -> new IllegalArgumentException("없는 상품입니다."));
-
-        if (cartRepository.existsByUserAndFproduct(user, fProduct)) {
-            return "failed"; //고민 해보기. 예외 던지기?
-        }
+                .orElseThrow(() -> new AppException(ErrorCode.PRODUCT_NOT_FOUND, fproductId + "존재하지 않는 상품입니다."));
 
         EntityCart cart = EntityCart.builder()
                 .user(user)
@@ -50,16 +49,15 @@ public class CartServiceImpl implements CartService {
 
         cartRepository.save(cart);
 
-        return "success"; //상태코드 반환? ex)201
+        return "success";
     }
 
     @Override
     public List<CartResponseDTO> selectCartList(int page, Authentication authentication) {
         EntityUser user = getUser(authentication);
 
-        if (page < 1) { //예외 정하기
-//            throw new Exception("page는 1 이상이여야 합니다.");
-            return null;
+        if (page < 1) {
+            throw new AppException(ErrorCode.PAGE_INDEX_ZERO, "Page가 1보다 작습니다.");
         }
         PageRequest pageRequest = PageRequest.of(page - 1, 10, Sort.by("cartId").descending());
 
@@ -77,7 +75,7 @@ public class CartServiceImpl implements CartService {
         List<EntityCart> cartList = cartDeleteRequestDTO.toEntity();
 
         cartList.forEach(cart -> cartRepository.findByUserAndCartId(user, cart.getCartId())
-                .orElseThrow(() -> new IllegalArgumentException("없는 상품입니다.")));
+                .orElseThrow(() -> new AppException(ErrorCode.CART_NOT_FOUND, cart.getCartId() + "존재하지 않는 장바구니 상품입니다.")));
 
         cartRepository.deleteAll(cartList);
 
@@ -97,6 +95,6 @@ public class CartServiceImpl implements CartService {
     private EntityUser getUser(Authentication authentication) {
         String userEmail = authentication.getName();
         return userRepository.findById(userEmail) //예외처리 어떻게 할지
-                .orElseThrow(() -> new IllegalArgumentException("없는 유저입니다."));
+                .orElseThrow(() -> new AppException(ErrorCode.USERMAIL_NOT_FOUND, userEmail + " 존재하지 않는 회원입니다."));
     }
 }
