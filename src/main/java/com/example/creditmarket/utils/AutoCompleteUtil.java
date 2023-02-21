b342de23f726eaf90e3a86c41183ca2864e53904
ThomasChoi <724thomas@gmail.com>
2023-02-21 21:16:19 +0900

    Feat : AutoCompleteService 자동완성기능(안쓰일수도)

--- src/main/java/com/example/creditmarket/utils/AutoCompleteUtil.java
+++ src/main/java/com/example/creditmarket/utils/AutoCompleteUtil.java
@@ -1,37 +1,27 @@
-package com.example.creditmarket.service;
-
-import com.example.creditmarket.repository.FProductRespository;
-import com.example.creditmarket.repository.OptionRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Required;
-import org.springframework.stereotype.Service;
+package com.example.creditmarket.utils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Service
-@RequiredArgsConstructor
-public class TrieNode {
+public class AutoCompleteUtil {
 
-    private final FProductRespository fProductRespository;
-    private final OptionRepository optionRepository;
-    private Map<Character, TrieNode> children;
+    private Map<Character, AutoCompleteUtil> children;
     private boolean isEndOfWord;
 
 
-    public TrieNode() {
+    public AutoCompleteUtil() {
         this.children = new HashMap<>();
         this.isEndOfWord = false;
     }
 
     public void addWord(String word) {
-        TrieNode current = this;
+        AutoCompleteUtil current = this;
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
             if (!current.children.containsKey(ch)) {
-                current.children.put(ch, new TrieNode());
+                current.children.put(ch, new AutoCompleteUtil());
             }
             current = current.children.get(ch);
         }
@@ -39,7 +29,7 @@ public class TrieNode {
     }
 
     public List<String> getWordsWithPrefix(String prefix) {
-        TrieNode current = this;
+        AutoCompleteUtil current = this;
         List<String> words = new ArrayList<>();
         for (int i = 0; i < prefix.length(); i++) {
             char ch = prefix.charAt(i);
@@ -52,13 +42,13 @@ public class TrieNode {
         return words;
     }
 
-    private void getWords(TrieNode node, StringBuilder prefix, List<String> words) {
+    private void getWords(AutoCompleteUtil node, StringBuilder prefix, List<String> words) {
         if (node.isEndOfWord) {
             words.add(prefix.toString());
         }
-        for (Map.Entry<Character, TrieNode> entry : node.children.entrySet()) {
+        for (Map.Entry<Character, AutoCompleteUtil> entry : node.children.entrySet()) {
             char ch = entry.getKey();
-            TrieNode child = entry.getValue();
+            AutoCompleteUtil child = entry.getValue();
             prefix.append(ch);
             getWords(child, prefix, words);
             prefix.deleteCharAt(prefix.length() - 1);
