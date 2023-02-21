264c73e168a3e627d5dfec976b88b2fcda88b768
JeYeongR <wpdud2003@gmail.com>
2023-02-21 21:31:52 +0900

    feat: #36 checkedFavorite 메서드 삭제

--- src/main/java/com/example/creditmarket/dto/OrderResponseDTO.java
+++ src/main/java/com/example/creditmarket/dto/OrderResponseDTO.java
@@ -18,17 +18,11 @@ public class OrderResponseDTO {
 
     private String fproductCreditProductTypeName;
 
-    private int favorite; //int 1:0, boolean true:false 고민중
-
     public OrderResponseDTO(EntityOrder order) {
         this.orderId = order.getOrderId();
         this.orderStatus = order.getOrderStatus();
         this.fproductCompanyName = order.getFproduct().getFproduct_company_name();
         this.fproductName = order.getFproduct().getFproduct_name();
         this.fproductCreditProductTypeName = order.getFproduct().getFproduct_credit_product_type_name();
-    }
-
-    public void setFavorite(int favorite) {
-        this.favorite = favorite;
     }
 }
