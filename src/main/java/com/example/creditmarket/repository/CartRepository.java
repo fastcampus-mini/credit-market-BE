7f61bd3b068a77513dd3fc09d408ad5b3363f566
JeYeongR <wpdud2003@gmail.com>
2023-02-23 00:30:10 +0900

    feat: 사용 안 하는 메서드 삭제

--- src/main/java/com/example/creditmarket/repository/CartRepository.java
+++ src/main/java/com/example/creditmarket/repository/CartRepository.java
@@ -1,9 +1,7 @@
 package com.example.creditmarket.repository;
 
 import com.example.creditmarket.entity.EntityCart;
-import com.example.creditmarket.entity.EntityFProduct;
 import com.example.creditmarket.entity.EntityUser;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,9 +11,7 @@ import java.util.Optional;
 @Repository
 public interface CartRepository extends JpaRepository<EntityCart, Long> {
 
-    List<EntityCart> findByUser(EntityUser user, Pageable pageable);
+    List<EntityCart> findByUserOrderByCartIdDesc(EntityUser user);
 
     Optional<EntityCart> findByUserAndCartId(EntityUser user, Long cartId);
-
-    boolean existsByUserAndFproduct(EntityUser user, EntityFProduct fProduct);
 }
