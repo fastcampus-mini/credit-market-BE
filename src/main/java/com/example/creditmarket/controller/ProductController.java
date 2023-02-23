dbd846750dd184474ab21f6a928db437f111073e
JeYeongR <wpdud2003@gmail.com>
2023-02-23 20:30:19 +0900

    feat: #50 컨트롤러 파라미터 및 api 수정

--- src/main/java/com/example/creditmarket/controller/ProductController.java
+++ src/main/java/com/example/creditmarket/controller/ProductController.java
@@ -1,6 +1,6 @@
 package com.example.creditmarket.controller;
 
-import com.example.creditmarket.dto.request.OrderRequestDTO;
+import com.example.creditmarket.dto.request.OrderSaveRequestDTO;
 import com.example.creditmarket.dto.response.ProductDetailResponseDTO;
 import com.example.creditmarket.dto.response.RecommendResponseDTO;
 import com.example.creditmarket.service.ProductService;
@@ -22,33 +22,33 @@ public class ProductController {
 
     @ApiOperation(value = "상품 상세페이지", notes = "상품의 상세페이지를 보여줍니다.")
     @GetMapping("/item/{id}")
-    public ProductDetailResponseDTO itemDetail(@PathVariable String id, HttpServletRequest request){
+    public ProductDetailResponseDTO itemDetail(@PathVariable String id, HttpServletRequest request) {
         return productService.getProductDetail(id, request);
     }
 
     @ApiOperation(value = "상품 구매", notes = "상품을 구매(신청)합니다.")
-    @PostMapping("/order/{id}")
-    public String buyProduct(@PathVariable String id, Authentication authentication){
+    @PostMapping("/order")
+    public String buyProduct(@RequestBody OrderSaveRequestDTO requestDTO, Authentication authentication) {
         String userEmail = authentication.getName();
-        return productService.buyProduct(id, userEmail);
+        return productService.buyProduct(requestDTO, userEmail);
     }
 
     @ApiOperation(value = "상품 추천", notes = "회원의 선호하는 대출상품과 금리에 따라 상품을 추천해줍니다")
     @GetMapping("/recommend")
-    public List<RecommendResponseDTO> recommendList(HttpServletRequest request){
+    public List<RecommendResponseDTO> recommendList(HttpServletRequest request) {
         return productService.recommendList(request);
     }
 
     @ApiOperation(value = "찜하기", notes = "상품을 찜합니다.")
     @PostMapping("/favor/{id}")
-    public String addLike(@PathVariable String id, Authentication authentication){
+    public String addLike(@PathVariable String id, Authentication authentication) {
         String userEmail = authentication.getName();
         return productService.favoriteService(id, userEmail);
     }
 
     @ApiOperation(value = "찜 취소", notes = "상품의 찜을 취소합니다")
-    @DeleteMapping ("/favor/{id}")
-    public String cancelLike(@PathVariable String id, Authentication authentication){
+    @DeleteMapping("/favor/{id}")
+    public String cancelLike(@PathVariable String id, Authentication authentication) {
         String userEmail = authentication.getName();
         return productService.favoriteService(id, userEmail);
     }
