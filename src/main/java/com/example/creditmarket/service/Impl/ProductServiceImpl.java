d6c9f9fcdf7a0f36121c0a6277538b24c5182a62
ryusun <ryusunshine@gmail.com>
2023-02-23 20:04:38 +0900

    fix:ryusun - 오류수정

--- src/main/java/com/example/creditmarket/service/Impl/ProductServiceImpl.java
+++ src/main/java/com/example/creditmarket/service/Impl/ProductServiceImpl.java
@@ -1,18 +1,24 @@
 package com.example.creditmarket.service.Impl;
 
+import com.example.creditmarket.configuration.JwtFilter;
 import com.example.creditmarket.dto.request.FavoriteRequestDto;
 import com.example.creditmarket.dto.request.OrderRequestDTO;
 import com.example.creditmarket.dto.response.ProductDetailResponseDTO;
 import com.example.creditmarket.dto.response.RecommendResponseDTO;
-import com.example.creditmarket.entity.EntityFProduct;
-import com.example.creditmarket.entity.EntityFavorite;
-import com.example.creditmarket.entity.EntityOption;
-import com.example.creditmarket.entity.EntityUser;
+import com.example.creditmarket.entity.*;
+import com.example.creditmarket.exception.AppException;
+import com.example.creditmarket.exception.ErrorCode;
 import com.example.creditmarket.repository.*;
 import com.example.creditmarket.service.ProductService;
+import com.example.creditmarket.utils.JwtUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,14 +32,21 @@ public class ProductServiceImpl implements ProductService {
     private final OptionRepository optionRepository;
     private final FavoriteRepository favoriteRepository;
 
+    @Value("${jwt.token.secret}")
+    private String secretKey;
+
     /**
      * 상품 상세정보 출력(상품명, 개요, 대상, 한도, 금리, 찜 여부 등의 상세정보 출력)
      */
-    public ProductDetailResponseDTO getProductDetail(String id, String userEmail) {
-        EntityUser user = userRepository.findByUserEmail(userEmail).orElseThrow(
-                ()-> new IllegalArgumentException("해당 아이디를 찾을수 없습니다."));
+    public ProductDetailResponseDTO getProductDetail(String id, HttpServletRequest request) {
         EntityFProduct product = productRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 아이디를 찾을수 없습니다"));
         EntityOption option = optionRepository.findByProductId(id);
+        if (getEmailFromToken(request) == null){
+            return new ProductDetailResponseDTO(product, option, false);
+        }
+        String userEmail = getEmailFromToken(request);
+        EntityUser user = userRepository.findByUserEmail(userEmail).orElseThrow(
+                ()-> new IllegalArgumentException("해당 아이디를 찾을수 없습니다."));
         boolean isFavorite = favoriteRepository.existsByUserAndFproduct(user, product);
         return new ProductDetailResponseDTO(product, option, isFavorite);
     }
@@ -59,9 +72,12 @@ public class ProductServiceImpl implements ProductService {
     /**
      * 추천 게시글
      */
-    public List<RecommendResponseDTO> recommendList(String userEmail) {
+    public List<RecommendResponseDTO> recommendList(HttpServletRequest request) {
         List<RecommendResponseDTO> list = new ArrayList<>();
-        EntityUser user = userRepository.findById(userEmail).orElseThrow();
+        if (getEmailFromToken(request) == null){
+            return null;}
+        String userEmail = getEmailFromToken(request);
+        EntityUser user = userRepository.findById(userEmail).orElseThrow(()-> new IllegalArgumentException("해당 아이디를 찾을수 없습니다."));
         List<EntityFProduct> products = productRepository.findProductsByUserPref(user.getUserPrefCreditProductTypeName());
         for (EntityFProduct pr : products) {
             EntityOption op = optionRepository.findOptionByProductIdAndType(pr.getFproduct_id(), user.getUserPrefInterestType());
@@ -90,6 +106,17 @@ public class ProductServiceImpl implements ProductService {
         } catch (Exception e){
             e.printStackTrace();
             return "fail";
+        }
+    }
+
+    public String getEmailFromToken(HttpServletRequest request){
+        try {
+            String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+            String token = authorization.split(" ")[1].trim();;
+            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                    .getBody().get("userEmail", String.class);
+        } catch (Exception e){
+            return null;
         }
     }
 }
--- src/main/java/com/example/creditmarket/service/ProductService.java
+++ src/main/java/com/example/creditmarket/service/ProductService.java
@@ -3,15 +3,16 @@ package com.example.creditmarket.service;
 import com.example.creditmarket.dto.response.ProductDetailResponseDTO;
 import com.example.creditmarket.dto.response.RecommendResponseDTO;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public interface ProductService {
 
-    ProductDetailResponseDTO getProductDetail(String productId, String userEmail);
+    ProductDetailResponseDTO getProductDetail(String productId, HttpServletRequest request);
 
     String buyProduct(String productId,String userEmail);
 
-    List<RecommendResponseDTO> recommendList(String userEmail);
+    List<RecommendResponseDTO> recommendList(HttpServletRequest request);
 
     String favoriteService(String productId, String userEmail);
 }
