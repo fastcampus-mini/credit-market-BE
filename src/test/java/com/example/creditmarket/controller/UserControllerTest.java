9d8567a1f277c3cccb6223e4025898e25501bc81
724thomas <724thomas@gmail.com>
2023-02-25 03:02:07 +0900

    Fix : UserTestController - Added /userinfo and urls for other tests

--- src/test/java/com/example/creditmarket/controller/UserControllerTest.java
+++ src/test/java/com/example/creditmarket/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.example.creditmarket.controller;
 
 import com.example.creditmarket.dto.request.UserLoginRequestDTO;
 import com.example.creditmarket.dto.request.UserSignUpRequestDTO;
+import com.example.creditmarket.dto.response.LoginResponseDTO;
 import com.example.creditmarket.entity.EntityUser;
 import com.example.creditmarket.exception.AppException;
 import com.example.creditmarket.exception.ErrorCode;
@@ -10,13 +11,18 @@ import com.example.creditmarket.service.Impl.UserServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -46,7 +52,7 @@ class UserControllerTest {
 
 
     @Test
-    @DisplayName("회원가입 성공")
+    @DisplayName("/usersignup 회원가입 성공")
     @WithMockUser
     void signup() throws Exception{
         String userEmail = "test@Email.com";
@@ -69,7 +75,7 @@ class UserControllerTest {
 
 
     @Test
-    @DisplayName("회원가입 실패 - userName 중복")
+    @DisplayName("/usersignup 회원가입 실패 - userName 중복")
     @WithMockUser
     void signup_fail() throws Exception{
         String userEmail = "test@Email.com";
@@ -93,26 +99,40 @@ class UserControllerTest {
                 .andExpect(status().isConflict());
     }
 
-//    @Test
-//    @DisplayName("로그인 성공")
-//    @WithMockUser
-//    void login_success() throws Exception {
-//        String userEmail = "test@Email.com";
-//        String userPassword = "testPassword";
-//
-//        when(userServiceImpl.login(any(), any()))
-//                .thenReturn("token");
-//
-//        mockMvc.perform(post("/userlogin")
-//                        .with(csrf())
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsBytes(new UserLoginRequestDTO(userEmail, userPassword))))
-//                .andDo(print())
-//                .andExpect(status().isOk());
-//    }
+    @Test
+    @DisplayName("/userlogin 로그인 성공")
+    @WithMockUser
+    void login_success() throws Exception {
+
+        UserSignUpRequestDTO testUser = new UserSignUpRequestDTO(
+                "test@Email.com",
+                "testPassword",
+                "패캠",
+                "남",
+                "20010101",
+                "학생",
+                "마이너스한도대출",
+                "대출금리",
+                500L);
+        userServiceImpl.signup(testUser);
+
+        String userEmail = "test@Email.com";
+        String userPassword = "testPassword";
+
+
+        when(userServiceImpl.login(any(), any()))
+                .thenReturn(new LoginResponseDTO(userEmail, "token"));
+
+        mockMvc.perform(post("/userlogin")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserLoginRequestDTO(userEmail, userPassword))))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 
     @Test
-    @DisplayName("로그인 실패 - userName 없음")
+    @DisplayName("/userlogin 로그인 실패 - userName 없음")
     @WithMockUser
     void login_fail1() throws Exception {
         String userEmail = "test@Email.com";
@@ -130,7 +150,7 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("로그인 실패 - password 틀림")
+    @DisplayName("/userlogin 로그인 실패 - password 틀림")
     @WithMockUser
     void login_fail2() throws Exception {
         String userEmail = "test@Email.com";
@@ -148,7 +168,7 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("로그아웃 성공")
+    @DisplayName("/userlogout 로그아웃 성공")
     @WithMockUser
     void logout_success() throws Exception {
         mockMvc.perform(post("/userlogout")
@@ -159,7 +179,7 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("로그아웃 실패 - 로그인 안됨")
+    @DisplayName("/userlogout 로그아웃 실패 - 로그인 안됨")
     void logout_fail() throws Exception {
         mockMvc.perform(post("/userlogout")
                         .with(csrf())
@@ -169,7 +189,7 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("유저 비밀번호 확인")
+    @DisplayName("/userpasswordcheck 유저 비밀번호 확인")
     @WithMockUser
     void checkPassword_success() throws Exception {
         UserSignUpRequestDTO testUser = new UserSignUpRequestDTO(
@@ -207,7 +227,7 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("유저 비밀번호 실패 - password 틀림")
+    @DisplayName("/userpasswordcheck 유저 비밀번호 실패 - password 틀림")
     @WithMockUser
     void checkPassword_fail() throws Exception {
         UserSignUpRequestDTO testUser = new UserSignUpRequestDTO(
@@ -238,7 +258,7 @@ class UserControllerTest {
 
 
     @Test
-    @DisplayName("회원정보 수정 성공")
+    @DisplayName("/userinfoupdate 회원정보 수정 성공")
     @WithMockUser
     void update_success() throws Exception {
         UserSignUpRequestDTO testUser = new UserSignUpRequestDTO(
@@ -271,4 +291,46 @@ class UserControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk());
     }
+
+    @Test
+    @DisplayName("/userinfo 회원정보 요청 성공")
+    @WithMockUser
+    public void testGetUserInfo() {
+        UserSignUpRequestDTO testUser = new UserSignUpRequestDTO(
+                "test@Email.com",
+                "testPassword",
+                "패캠",
+                "남",
+                "20010101",
+                "학생",
+                "마이너스한도대출",
+                "대출금리",
+                500L);
+
+        userServiceImpl.signup(testUser);
+
+        String userEmail = "test@Email.com";
+        String userPassword = "testPassword";
+        String userName = "패캠";
+        String userGender = "남";
+        String userBirthDate = "20020101";
+        String userJob = "직장인";
+        String userPrefCreditProductTypeName = "마이너스한도대출";
+        String userPrefInterestType = "대출금리";
+        Long userCreditScore = 800L;
+
+        when(userServiceImpl.getUserInfo(any()))
+                .thenReturn(EntityUser.builder()
+                        .userEmail(userEmail)
+                        .userPassword(userPassword)
+                        .userName(userName)
+                        .userGender(userGender)
+                        .userBirthdate(userBirthDate)
+                        .userJob(userJob)
+                        .userPrefCreditProductTypeName(userPrefCreditProductTypeName)
+                        .userPrefInterestType(userPrefInterestType)
+                        .userCreditScore(userCreditScore)
+                        .build());
+    }
+
 }
