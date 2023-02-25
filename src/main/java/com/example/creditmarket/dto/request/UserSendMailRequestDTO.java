f87fd4c9da0f140c005d3fffb649fa7da62947ba
JeYeongR <wpdud2003@gmail.com>
2023-02-25 14:53:47 +0900

    feat: #66 UserSendMailRequestDTO 생성

---
+++ src/main/java/com/example/creditmarket/dto/request/UserSendMailRequestDTO.java
@@ -0,0 +1,11 @@
+package com.example.creditmarket.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserSendMailRequestDTO {
+
+    private String userEmail;
+}
